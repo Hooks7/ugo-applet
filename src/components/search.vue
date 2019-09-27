d307e56e53f32f795127a92c1ab40e88f3178737
Hooks7 <youremail@example.com>
2019-09-27 15:47:11 +0800

    ugo小程序

---
+++ src/components/search.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- 搜索 -->
+    <div class="search">
+      <input type="text" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang='less'>
+.search {
+  padding: 20rpx 18rpx;
+  background-color: #eb4450;
+  input {
+    height: 60rpx;
+    background-color: aliceblue;
+    border-radius: 5rpx;
+  }
+}
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <!-- 搜索 -->
+    <search/>
+    <!-- 轮播图 -->
+    <swiper
+      indicator-dots
+      indicator-color="rgba(255,255,255,3)"
+      indicator-active-color="#ccc"
+      autoplay
+      circular
+      interval='3000'
+    >
+      <swiper-item>
+        <a href>
+          <img src="/static/uploads/banner1.png" alt />
+        </a>
+      </swiper-item>
+      <swiper-item>
+        <a href>
+          <img src="/static/uploads/banner2.png" alt />
+        </a>
+      </swiper-item>
+      <swiper-item>
+        <a href>
+          <img src="/static/uploads/banner3.png" alt />
+        </a>
+      </swiper-item>
+    </swiper>
+  </div>
+</template>
+
+<script>
+import search from "@/components/search";
+
+export default {
+  data() {
+    return {};
+  },
+
+  components: {
+
+    search
+  },
+
+  methods: {},
+
+  created() {
+    // let app = getApp()
+  }
+};
+</script>
+
+<style scoped lang='less'>
+swiper {
+  height: 340rpx;
+  a {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
